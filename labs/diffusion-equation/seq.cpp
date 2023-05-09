4ccde8763f26e1d9e4581ad8f6e00912395349fd
c71n93 <korostinskii.r@phystech.edu>
2023-05-10 01:04:57 +0300

    attemp to build plot

--- labs/diffusion-equation/seq.cpp
+++ labs/diffusion-equation/seq.cpp
@@ -3,16 +3,61 @@
 #include "matrix.hpp"
 #include "diff-equation.h"
 
+#if 0
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
+#endif
+
 double phi(double x) {
     return std::cos(M_PI * x);
 }
-
 double psi(double t) {
     return std::exp(-t);
 }
-
 double f(double t, double x) {
     return x + t;
+}
+
+inline std::pair<std::vector<std::vector<double>>, std::vector<std::vector<double>>> meshgrid(const std::vector<double>& tVec,
+                                                                                       const std::vector<double>& xVec) {
+    std::vector<std::vector<double>> tGrid;
+    std::vector<std::vector<double>> xGrid;
+    for (const auto& t : tVec) {
+        std::vector<double> row;
+        for (const auto& x : xVec) {
+            row.push_back(x);
+        }
+        xGrid.push_back(row);
+    }
+    for (const auto& x : xVec) {
+        std::vector<double> row;
+        for (const auto& t : tVec) {
+            row.push_back(t);
+        }
+        tGrid.push_back(row);
+    }
+    return std::make_pair(tGrid, xGrid);
+}
+inline std::vector<double> makeVecFromInterval(double interval, int amount) {
+    std::vector<double> v(amount);
+    for (int i = 0; i < amount; i++) {
+        v.push_back(interval * i);
+    }
+    return v;
+}
+
+void makePlot(const Matrix<double>& matrix, double T, double X, int tLen, int xLen) {
+    double tau = T / (tLen - 1);
+    double h = X / (xLen - 1);
+    std::vector<double> tVec = makeVecFromInterval(tau, tLen);
+    std::vector<double> xVec = makeVecFromInterval(h, xLen);
+    auto gridPair = meshgrid(tVec, xVec);
+    auto matrixVec = matrix.ToVector();
+#if 0
+    plt::plot_surface(gridPair.first, gridPair.second, matrixVec);
+    plt::show();
+#endif
 }
 
 int main(int argc, char **argv) {
@@ -27,5 +72,8 @@ int main(int argc, char **argv) {
     DiffEquationSystem equationSystem(phi, psi, f, T, X, alpha);
     Matrix<double> matrix = equationSystem.SolveSystemRectScheme(tLen, xLen);
     matrix.Dump();
+#if 0
+    makePlot(matrix, T, X, tLen, xLen);
+#endif
     return 0;
 }
